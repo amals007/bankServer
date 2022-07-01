43920ab2a68c3ef9f8723b3a0fb1e4f17dc649ed
Amal S <102399747+amals007@users.noreply.github.com>
2022-07-02 01:22:45 +0530

    integration

--- index.js
+++ index.js
@@ -1,121 +1,115 @@
 //server creation
-const express = require('express')
+const express = require("express");
 
 // import jsonwebtoken
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 // import cors
-const cors = require('cors')
+const cors = require("cors");
 
 //import express
-const dataService = require('./services/data.service')
-
+const dataService = require("./services/data.service");
 
 // server application create using express
-const app = express()
+const app = express();
 
 // cors use in server app
-app.use(cors({
-    origin: 'http://localhost:4200'
-}))
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+  })
+);
 
 // user request resolving
 // parse JSON data
-app.use(express.json())
+app.use(express.json());
 
 // application specific middleware
-const appMiddleware = (req,res,next)=>{
-    console.log("Application specific middleware");
-    next()
-}
+const appMiddleware = (req, res, next) => {
+  console.log("Application specific middleware");
+  next();
+};
 // use middleware in app
-app.use(appMiddleware)
+app.use(appMiddleware);
 
-const jwtMiddleware = (req,res,next)=>{
-    // fetch token
-  try{ 
-    token = req.headers['x-access-token']
+const jwtMiddleware = (req, res, next) => {
+  // fetch token
+  try {
+    token = req.headers["x-access-token"];
     // verify token
-   const data =  jwt.verify(token,'supersecretkey12345')
-   console.log(data)
-   next()
-  }
-  catch{
+    const data = jwt.verify(token, "supersecretkey12345");
+    console.log(data);
+    next();
+  } catch {
     res.status(401).json({
-        status:false,
-        statusCode:401,
-        message:'Please Login'
-    })
+      status: false,
+      statusCode: 401,
+      message: "Please Login",
+    });
   }
-
-}
-
+};
 
 // bank server
 //register API
-app.post('/register',(req,res)=>{
-    //register resolving - asynchronous
-    dataService.register(req.body.username,req.body.acno,req.body.password)
-    .then(result => {
-        res.status(result.statusCode).json(result)
-    })
-     
-})
+app.post("/register", (req, res) => {
+  //register resolving - asynchronous
+  dataService
+    .register(req.body.username, req.body.acno, req.body.password)
+    .then((result) => {
+      res.status(result.statusCode).json(result);
+    });
+});
 //login API
-app.post('/login',(req,res)=>{
-    //login resolving
-    dataService.login(req.body.acno,req.body.pswd)
-    .then(result => { 
-      res.status(result.statusCode).json(result)
-    })
-})
+app.post("/login", (req, res) => {
+  //login resolving
+  dataService.login(req.body.acno, req.body.pswd).then((result) => {
+    res.status(result.statusCode).json(result);
+  });
+});
 //deposit API
-app.post('/deposit',jwtMiddleware,(req,res)=>{
-
-    dataService.deposit(req.body.acno,req.body.password,req.body.amt)
-    .then(result => {
-        res.status(result.statusCode).json(result)
-    })
-    
-})
+app.post("/deposit", jwtMiddleware, (req, res) => {
+  dataService
+    .deposit(req.body.acno, req.body.password, req.body.amt)
+    .then((result) => {
+      res.status(result.statusCode).json(result);
+    });
+});
 // withdraw API
-app.post("/withdraw",jwtMiddleware,(req,res) => {
-    dataService.withdraw(req.body.acno,req.body.password,req.body.amt)
-    .then(result => {
-        res.status(result.statusCode).json(result)
-    })
-   
-})
+app.post("/withdraw", jwtMiddleware, (req, res) => {
+  dataService
+    .withdraw(req.body.acno, req.body.password, req.body.amt)
+    .then((result) => {
+      res.status(result.statusCode).json(result);
+    });
+});
 // transaction API
-app.post('/transaction',jwtMiddleware,(req,res) => {
-    dataService.getTransaction(req.body.acno)
-    .then(result => {
-        res.status(result.statusCode).json(result)
-    })
-})
+app.post("/transaction", jwtMiddleware, (req, res) => {
+  dataService.getTransaction(req.body.acno).then((result) => {
+    res.status(result.statusCode).json(result);
+  });
+});
 
 //GET REQUEST - to fetch data
-app.get('/',(req,res)=>{
-    res.send("GET Request")
-})
+app.get("/", (req, res) => {
+  res.send("GET Request");
+});
 // POST REQUEST - to create data
-app.post('/',(req,res)=>{
-    res.send("POST Request")
-})
+app.post("/", (req, res) => {
+  res.send("POST Request");
+});
 // PUT REQUEST - to modify entire data
-app.put('/',(req,res)=>{
-    res.send("PUT Request")
-})
+app.put("/", (req, res) => {
+  res.send("PUT Request");
+});
 // PATCH REQUEST - to modify partially
-app.patch('/',(req,res)=>{
-    res.send("PATCH Request")
-})
+app.patch("/", (req, res) => {
+  res.send("PATCH Request");
+});
 // DELETE REQUEST - to delete data
-app.delete('/',(req,res)=>{
-    res.send("DELETE Request")
-})
-
+app.delete("/", (req, res) => {
+  res.send("DELETE Request");
+});
 
 // set up the port number to the server app
-app.listen(3000,()=>{
-    console.log("Server started at 3000");
-})
+app.listen(3000, () => {
+  console.log("Server started at 3000");
+});
